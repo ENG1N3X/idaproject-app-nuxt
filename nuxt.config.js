23fbbc66ccead91a67101fc3447db02d347f3944
eng1n3x <eng1n3x.developer@gmail.com>
2022-08-23 17:36:00 +0300

    feat: project renamed

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'idaproject-app-nuxt',
+    title: 'products-app-nuxt',
     htmlAttrs: {
       lang: 'en',
     },
